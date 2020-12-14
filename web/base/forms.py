8e8199359cfda180794a699df8e976e36101f499
OPI-py <oleksandrp09@gmail.com>
2020-12-14 18:13:38 +0200

    Add imput_form view, form, template

--- web/base/forms.py
+++ web/base/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 
 
-class InputForm(firms.Form):
+class InputForm(forms.Form):
 	first_name = forms.CharField(max_length = 200)
 	last_name = forms.CharField(max_length = 200)
 	r_n = forms.IntegerField(help_text = "Enter 6 digit number")
 	password = forms.CharField(widget=forms.PasswordInput())
-	
--- web/base/views.py
+++ web/base/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .forms import InputForm
 
 def input_form_view(request):
-	context = []
+	context = {}
 	context['form'] = InputForm()
 	return render(request, 'home.html', context)
