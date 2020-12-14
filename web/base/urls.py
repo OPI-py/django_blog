cad26ca57ffb5188738ec0a65b310cfaf0bb1ddf
OPI-py <oleksandrp09@gmail.com>
2020-12-14 18:34:10 +0200

    Change path

--- web/base/urls.py
+++ web/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-	path('', views.input_form_view, name='input_form')
+	path('register/', views.input_form_view, name='input_form')
 
 ]
 
