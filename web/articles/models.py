c20cdf0007181b3cb9641632ccf02a5e414262e9
OPI-py <oleksandrp09@gmail.com>
2020-12-15 00:23:18 +0200

    Create article model

--- web/articles/models.py
+++ web/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+	title = models.CharField(max_length=100)
+	slug = models.SlugField()
+	text = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+	# add author
+	
+	def __str__(self):
+		return self.title
