79c91e67d0ce4432ef0006fd0750107cdaa389ff
OPI-py <oleksandrp09@gmail.com>
2020-12-20 17:27:54 +0200

    Stage media settings

--- web/web/urls.py
+++ web/web/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,3 +27,5 @@ urlpatterns = [
     ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
