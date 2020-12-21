b929684dc6bf9410ecd00e74a32613ccbc5145ac
OPI-py <oleksandrp09@gmail.com>
2020-12-21 12:08:26 +0200

    Make article create view

--- web/articles/views.py
+++ web/articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 def article_list(request):
 	articles = Article.objects.all().order_by('date')
@@ -13,6 +14,13 @@ def article_detail(request, slug):
 	article = Article.objects.get(slug=slug)
 	return render(request, 'articles/article_detail.html', {'article':article})
 
+# Check if user logged in, sent to login page
 @login_required(login_url="/accounts/login")
 def article_create(request):
-	return render(request, 'articles/article_create.html')
+	if request.method == 'POST':
+		form = forms.CreateArticle(request.POST, request.FILES)
+		if form.is_valid():
+			return redirect('articles:list')
+	else:
+		form = forms.CreateArticle()
+	return render(request, 'articles/article_create.html', {'form': form})
