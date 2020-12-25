3bf1de7797eec215532bddc425d859e0ea84a313
OPI-py <oleksandrp09@gmail.com>
2020-12-25 16:43:36 +0200

    Add pagination to article_list

--- web/articles/views.py
+++ web/articles/views.py
@@ -3,14 +3,19 @@ from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
+from django.core.paginator import Paginator
+
 
 def article_list(request):
-	articles = Article.objects.all().order_by('date')
-	return render(request,
-		'articles/article_list.html', {'articles' : articles})
+	articles = Article.objects.all().order_by('-date')
+	paginator = Paginator(articles, 6)
+
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+	return render(request, 'articles/article_list.html',
+		{'page_obj': page_obj})
 
 def article_detail(request, slug):
-	#return HttpResponse(slug)
 	article = Article.objects.get(slug=slug)
 	return render(request, 'articles/article_detail.html', {'article':article})
 
