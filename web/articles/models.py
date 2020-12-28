78e072763a5ec490578b29c89d8260282753a0de
OPI-py <oleksandrp09@gmail.com>
2020-12-28 13:13:09 +0200

    Import froala_editor, add FroalaField instead TextField

--- web/articles/models.py
+++ web/articles/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
 
 
 class Article(models.Model):
 	title = models.CharField(max_length=100)
 	slug = models.SlugField()
-	text = models.TextField()
+	text = FroalaField()
 	date = models.DateTimeField(auto_now_add=True)
 	thumb = models.ImageField(default='default.png', blank=True)
 	author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
